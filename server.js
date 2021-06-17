19b11fd607e160efbd36e14e662a4927b429504c
Hritik Raj <hr31cr2rathore@gmail.com>
2021-06-17 23:08:05 +0530

    Modified PORT to process.env.PORT

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname+'/assets'));
 app.get('/', (req, res)=>{
     res.sendFile(__dirname+'/index.html');
 });
-
-app.listen(3000, ()=>{
-    console.log(`Server is listening on port 3000...`);
+process.env.PORT = process.env.PORT || 3000;
+app.listen(process.env.PORT, process.env.IP, ()=>{
+    console.log(`Server is listening on port ${process.env.PORT}...`);
 });
